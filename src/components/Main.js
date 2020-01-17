6a29f4589bd4b94bacaa309adee01807fb651b2e
Justin Perez <justin@startrev.com>
2020-01-16 16:53:47 -0800

    Updates at Introduction, Skills

--- src/components/Main.js
+++ src/components/Main.js
@@ -23,10 +23,11 @@ class Main extends React.Component {
 
           <h2 className="major">Introduction</h2>
           <h3>Who I Am</h3>
-          <p>Bests as CEO, Founder, and President of <a className="gold" href="https://startrev.com" target="_blank">Start Rev Technology</a>; The Los Angeles tech start up, designing precision-engineered web solutions for established companies and business professionals.</p>
-          <p>With over 4 years of working experience, I specialize in Ecommerce and Web Technologies. Being skilled in user interface /experience design, SEO best practices, and technologies such as Git, HTML5/CSS3/JavaScript, Bootstrap, Analytics Software and APIs.
-          Connect on  <a className="gold" href="https://www.linkedin.com/in/jjustinperez" target="_blank">LinkedIn</a>.</p>
+          <p>I am a full stack engineer, specializing in Ecommerce at 4 years' experience. Well versed in user interface design, CMS, SEO and technologies e.g., Git, HTML5, CSS3, JavaScript, Bootstrap, Analytics Software and APIs.
+          My role spans an innovator and leader, drawing clients from around the world, in addition to being bilingual in English and Spanish.
+          Feel free to connect on my professional network  <a className="gold" href="https://www.linkedin.com/in/jjustinperez" target="_blank">LinkedIn</a>.</p>
 
+          <p>Lastly, standing as CEO, Founder, and President of <a className="gold" href="https://startrev.com" target="_blank">Start Rev Technology</a>; The Los Angeles tech start up, engineering transformative, customer-centric business solutions in great precision.</p>
           <span className="image main intro">
             <img src={pic01} alt="" />
           </span>
@@ -42,13 +43,13 @@ class Main extends React.Component {
           <h2 className="major">Skills and Expertise</h2>
 
           <ul className="service-list">
-            <li><h3>Frameworks and Languages I Speak: </h3><p> HTML5, CSS3, JavaScript, Bootstrap, PHP, Ruby and Liquid</p></li>
-            <li><h3>APIs and Libraries: </h3><p> jQuery, Google APIs, Maps API, Instagram API, Shopify REST API, Youtube API</p></li>
-            <li><h3>Preferred Software and Tools: </h3><p> Git /GitHub, NPM, Google Analytics, SEO and Speed Optimization Tools</p></li>
-            <li><h3>Platforms: </h3><p> Shopify, WordPress, WooComerce, Google Cloud Platform, Trello, MailChimp, Zendesk</p></li>
+            <li><h3>Frameworks and Languages</h3><p> HTML5, CSS3, JavaScript, Bootstrap, PHP, Ruby and Liquid</p></li>
+            <li><h3>APIs and Libraries</h3><p> jQuery, Google APIs, Maps API, Instagram API, Shopify REST API, Youtube API</p></li>
+            <li><h3>Software and Tools</h3><p> Git /GitHub, NPM, Google Analytics, SEO and Speed Optimization Tools</p></li>
+            <li><h3>Platforms</h3><p> Shopify, WordPress, WooComerce, Google Cloud Platform, MailChimp, Zendesk</p></li>
           </ul>
           <ul className="service-list">
-            <li><h3>Tech Skills:</h3><p>Search Engine Optimization (SEO), Ecommerce Development, User Interface Design, User Experience (UX), Package Managers, Task Runners, Analytics Software, and APIs</p></li>
+            <li><h3>Additional</h3><p>Search Engine Optimization (SEO), Ecommerce Development, User Interface Design, User Experience (UX), Package Managers, Task Runners, Analytics Software, and APIs</p></li>
           </ul>
           {close}
         </article>
@@ -60,7 +61,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}>
 
           <h2 className="major">Work Portfolio</h2>
-          <p>Is up ahead</p>
+          <p></p>
           <span className="image main work">
             <img src={pic02} alt="" />
           </span>
@@ -74,7 +75,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}>
 
           <h2 className="major">Contact Me</h2>
-          <p>Complete the form and see how we could work together.<br/> Direct message <a className="gold" href="mailto:justin@startrev.com">justin@startrev.com</a></p>
+          <p>See how we could work together by completing the form.<br/> Direct message <a className="gold" href="mailto:justin@startrev.com">justin@startrev.com</a></p>
           <form method="post" action="#" accept-charset="utf-8">
             <div className="field">
               <label htmlFor="name">First and Last Name*</label>
