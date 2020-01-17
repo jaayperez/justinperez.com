7c18a69f4412b5a12b0f1d58ed88b9cf32342927
Justin Perez <justin@startrev.com>
2020-01-16 16:46:22 -0800

    Minor updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Justin Perez | CEO, Founder, and President | Los Angeles, California",
+    title: "Justin Perez | CEO and Full Stack Engineer | Los Angeles, California",
     author: "Justin Perez",
-    description: ""
+    description: "I design, build, and launch websites and online applications. Explore my work, view my skills, or get in contact"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
