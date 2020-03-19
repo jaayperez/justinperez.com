221137bac0f16fc46b7c57875368eb1f8f40ffa5
Justin Perez <justin@startrev.com>
2020-03-18 21:45:21 -0700

    Update copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-      <p className="copyright">&copy; 2020 Justin Perez - Los Angeles, California</p>
+      <p className="copyright">&copy; 2020 Justin Perez</p>
     </footer>
 )
 
