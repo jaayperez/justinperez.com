d1626f073493f651aa368c0c68bb9768aedc5881
Justin Perez <justin@startrev.com>
2020-04-22 22:32:22 -0700

    Update writing and add Twitter link to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,34 +2,40 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Header = (props) => (
+
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
-        <div className="logo">
-            <span className="icon fa-diamond"></span>
-        </div>
         <div className="content">
             <div className="inner">
-                <h1>Justin Perez</h1>
-                <h2>Full Stack Web Developer</h2>
-                <p>Developing custom websites and applications in Los Angeles, CA.
-                <br/>Need a quote on your project or just have a question shoot me an email</p>
+            <div className="inner">
+            <div className="logo">
+            <span className="icon fa-diamond"></span>
+            </div>
+            <h2>Justin Perez</h2>
+            <p style={{textAlign: 'left'}}>Full Stack Web Developer using Node.js and React to develop and build custom websites and applications.
+            If you need a project quote or have a question just shoot an email <a href="mailto:justin@startrev.com">justin@startrev.com</a></p>
+            <br/>
+            <nav>
+                <ul>
+                    <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                    <li><a href="javascript:;" onClick={() => {props.onOpenArticle('skills')}}>Skills</a></li>
+                    <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                    <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                </ul>
+            </nav>
+            <br />
+            </div>
                 <ul className="icons">
-                  <li><a href="https://github.com/jaayperez" className="icon fa-github" target="_blank" rel="noopener noreferrer">
-                    <span className="label">GitHub</span></a></li>
-                  <li><a href="https://www.linkedin.com/in/jjustinperez?trk=profile-badge" className="icon fa-linkedin" target="_blank" rel="noopener noreferrer">
-                    <span className="label">LinkedIn</span></a></li>
-                  <li><a href="https://www.instagram.com/jaay.justin" className="icon fa-instagram" target="_blank" rel="noopener noreferrer">
-                    <span className="label">Instagram</span></a></li>
+                <li><a href="https://github.com/jaayperez" className="icon fa-github" target="_blank" rel="noopener noreferrer">
+                  <span className="label">GitHub</span></a></li>
+                <li><a href="https://www.linkedin.com/in/jjustinperez?trk=profile-badge" className="icon fa-linkedin" target="_blank" rel="noopener noreferrer">
+                  <span className="label">LinkedIn</span></a></li>
+                <li><a href="https://www.twitter.com/jaayperez" className="icon fa-twitter" target="_blank" rel="noopener noreferrer">
+                  <span className="label">Twitter</span></a></li>
+                <li><a href="https://www.instagram.com/jaay.justin" className="icon fa-instagram" target="_blank" rel="noopener noreferrer">
+                  <span className="label">Instagram</span></a></li>
                 </ul>
             </div>
         </div>
-        <nav>
-            <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Introduction</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('skills')}}>Skills</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
-            </ul>
-        </nav>
     </header>
 )
 
