5ab55e4a27db05893b9e9b6a762c1e468abc68fe
Justin Perez <justin@startrev.com>
2020-06-22 00:08:46 -0700

    Update copywriting

--- src/components/Main.js
+++ src/components/Main.js
@@ -22,15 +22,14 @@ class Main extends React.Component {
             this.props.articleTimeout ? 'timeogaut' : ''
           }`}
           style={{ display: 'none' }}>
-          <h2 className="major">About</h2>
+          <h2 className="major">About Me</h2>
           <h3>Who I Am</h3>
-          <p><span className="image left"><img src={pic00} alt="Justin Perez from Los Angeles, CA" /></span>I'm Justin Perez and I'm a full stack web developer from Los Angeles, CA
-          with four years of experience in SEO and frontend development. Proven track record of professionalism combined with effective collaboration skills and results.  Well versed in technologies such as Git, Shopify Liquid, Bootstrap 4, web APIs, and analytics software.
-          Right now I'm using Node.js and React to design and build noice websites.
-          Feel free to connect with my professional network <a className="gold" href="https://www.linkedin.com/in/jjustinperez?trk=profile-badge" target="_blank" rel="noopener noreferrer">LinkedIn</a>.
-          Have a project idea you'd like to discuss? Let's talk <a className="gold" href="mailto:justin@startrev.com">justin@startrev.com</a></p>
+          <p><span className="image left"><img src={pic00} alt="I'm Justin Perez, freelance full stack web developer based in Los Angeles, CA." /></span>My name is Justin Perez and I am a freelance full-stack web developer in Los Angeles, CA. who specializes in the design and build of custom websites and applications using Node.js and React.
+          Self-taught programmer with over four years of experience in Shopify and front/backend development. Well versed in technologies such as HTML5/CSS3/JavaScript, Liquid, Bootstrap, Git, Web APIs, and Google Analytics software. Proven track record of professionalism combined with results and very adept when it comes to eCommerce and SEO.
+          Goal-oriented and driven team player. Feel free to connect to my <a className="gold" href="https://www.linkedin.com/in/jjustinperez?trk=profile-badge" target="_blank" rel="noopener noreferrer">LinkedIn</a> for professional, social and career networking.
+          Have a project idea you'd like to discuss? Email at <a className="gold" href="mailto:justin@startrev.com">justin@startrev.com</a></p>
           <span className="image main intro">
-            <img src={pic01} alt="My Introduction" style={{ width: '50%' }} />
+            <img src={pic01} alt="About Who I Am" style={{ width: '50%' }} />
           </span>
           {close}
         </article>
@@ -40,15 +39,15 @@ class Main extends React.Component {
             this.props.articleTimeout ? 'timeout' : ''
           }`}
           style={{ display: 'none' }}>
-          <h2 className="major">Skills</h2>
+          <h2 className="major">My Skills</h2>
           <ul className="service-list">
-            <li><h3>Framework and Languages</h3><p> HTML5/CSS3/JavaScript, Bootstrap, Sass, React, Node.js, Shopify Liquid, PHP, Ruby</p></li>
-            <li><h3>API and Libraries</h3><p> jQuery, Google APIs, Instagram API, Maps JS API, Shopify REST API, Youtube API</p></li>
-            <li><h3>Software and Tools</h3><p> Git and GitHub, Google Analytics, NPM, SEO and Speed/Performance Optimization Tools</p></li>
-            <li><h3>Platforms</h3><p> Google Cloud Platform, MailChimp, Shopify, WordPress, WooComerce, Zendesk</p></li>
+            <li><h3>Framework and Languages</h3><p> PHP, Ruby, Liquid, Node, React, Bootstrap, Sass, HTML5/CSS3/JavaScript</p></li>
+            <li><h3>API and Libraries</h3><p> jQuery, Google APIs, Social APIs, Maps APIs, Shopify REST APIs</p></li>
+            <li><h3>Software and Tools</h3><p> Git and GitHub, Google Analytics Software, NPM Package Managers, Speed/Performance Optimization Tools</p></li>
+            <li><h3>Platforms</h3><p> AWS, Google Cloud Platform, MailChimp, Shopify, WordPress, WooComerce, Zendesk</p></li>
           </ul>
           <ul className="service-list">
-            <li><h3>Additional</h3><p>Search Engine Optimization, Data Integration, Ecommerce Development, Interface Design, User Experience Design, Package Managers, Task Runners, Analytics Software</p></li>
+            <li><h3>Additional</h3><p>Search Engine Optimization, Data Integration, Ecommerce Development, Interface Design, User Experience Design</p></li>
           </ul>
           {close}
         </article>
@@ -59,8 +58,8 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}>
 
-          <h2 className="major">Work</h2>
-          <h3>See the latest projects from my portfolio</h3>
+          <h2 className="major">My Work</h2>
+          <h3>See the latest projects on my portfolio</h3>
           <Wrapper as={Container} id="projects">
             <Grid>
                 <Item>
@@ -69,7 +68,7 @@ class Main extends React.Component {
                       <h4><a className="gold" href="https://personalspacemb.com" target="_blank" rel="noopener noreferrer">Shopify Website</a></h4>
                       <p>Shopify Ecommerce Website</p>
                       <div>
-                        <span>HTML, CSS, JavaScript and Liquid</span>
+                        <span>Liquid, HTML, CSS, JavaScript</span>
                       </div>
                     </Content>
                     </Card>
@@ -78,7 +77,7 @@ class Main extends React.Component {
                   <Card style={{ opacity: '0.9' }}>
                     <Content>
                       <h4><a className="gold" href="https://github.com/jaayperez/shopify-node-react-app" target="_blank" rel="noopener noreferrer">Shopify App</a></h4>
-                      <p>Shopify Application</p>
+                      <p>Shopify Product Discounts Application</p>
                       <div>
                         <span>React, Node.js, Next.js, and GraphQL</span>
                       </div>
@@ -99,10 +98,10 @@ class Main extends React.Component {
                 <Item>
                   <Card style={{ opacity: '0.9' }}>
                     <Content>
-                      <h4><a className="gold" href="https://gaytanleevancpa.com" target="_blank" rel="noopener noreferrer">React Website</a></h4>
+                      <h4><a className="gold" href="https://gaytanleevancpa.com" target="_blank" rel="noopener noreferrer">Business Website</a></h4>
                       <p>CPA Firm Website</p>
                       <div>
-                        <span>React and Bootstrap</span>
+                        <span>React, Bootstrap, HTML5, CSS3 Animations</span>
                       </div>
                     </Content>
                   </Card>
@@ -113,7 +112,7 @@ class Main extends React.Component {
                       <h4><a className="gold" href="https://jaayperez.github.io/ecommerce-dashboard" target="_blank" rel="noopener noreferrer">Dashboard App</a></h4>
                       <p>Ecommerce Dashboard Application</p>
                       <div>
-                        <span>React, Google API, and Material UI</span>
+                        <span>React, Chart.js, Sheets API, and Material UI</span>
                       </div>
                   </Content>
                   </Card>
@@ -124,7 +123,7 @@ class Main extends React.Component {
                       <h4><a className="gold" href="https://github.com/jaayperez/spotify-react-native-app" target="_blank" rel="noopener noreferrer">Spotify App</a></h4>
                       <p>Spotify Music Listing Application</p>
                       <div>
-                        <span>React Native and Spotify Web API</span>
+                        <span>React Native + Spotify Web API</span>
                       </div>
                     </Content>
                   </Card>
@@ -140,9 +139,9 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}>
 
-          <h2 className="major">Contact</h2>
+          <h2 className="major">Contact Me</h2>
           <p>So you are looking for a professional, communicative and punctual software developer who likes to keep productive.
-          Send me an email about your project at <a className="gold" href="mailto:justin@startrev.com">justin@startrev.com</a> or fill out this form</p>
+          Email at <a className="gold" href="mailto:justin@startrev.com">justin@startrev.com</a></p>
           <form method="post" action="#" acceptCharset="utf-8">
             <div className="field">
               <label htmlFor="name">First and Last Name*</label>
